383cd11d2c2fa456491a7b5984144ff7e43f2eb1
Roman <kampfflummi@gmx.de>
2015-08-24 18:01:23 +0200

    Player Select Screen

---
+++ Assets/UI/Player Select/PlayerSelectScreen.cs
@@ -0,0 +1,193 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerSelectScreen : MonoBehaviour
+{
+
+    List<InputDevice> potentialInputDevices;
+    List<InputDevice> inputsInUse;
+
+
+    [SerializeField]
+    SingleSelector singleSelectorOriginal;
+
+    List<SingleSelector> allSelectors;
+
+    public const int joinKey = 0;
+    public const int leaveKey = 1;
+    public const int startGameKey = 7;
+
+    public string gameSceneToStartAfterPlayerSelect;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // initialize selectors
+        allSelectors = new List<SingleSelector>();
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject newSelector = Instantiate(singleSelectorOriginal.gameObject) as GameObject;
+
+            newSelector.transform.parent = singleSelectorOriginal.transform.parent;
+            newSelector.transform.localScale = singleSelectorOriginal.transform.localScale;
+
+            SingleSelector newSelectorScript = newSelector.GetComponent<SingleSelector>();
+            newSelectorScript.Initialize(i, this);
+            newSelectorScript.Hide();
+            allSelectors.Add(newSelectorScript);
+        }
+        singleSelectorOriginal.gameObject.SetActive(false);
+
+
+
+
+        // collect possible input devices
+        potentialInputDevices = new List<InputDevice>();
+        potentialInputDevices.Add(new KeyboardInputDevice());
+        for (int i = 1; i <= 4; i++)
+        {
+            potentialInputDevices.Add(new XBox360InputDevice(i));
+        }
+
+        inputsInUse = new List<InputDevice>();
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // add players
+        for (int i = 0; i < potentialInputDevices.Count; i++)
+        {
+            if (potentialInputDevices[i].GetButtonDown(joinKey))
+            {
+                TryToAddPlayer(potentialInputDevices[i]);
+            }
+        }
+
+        // start game
+        for (int i = 0; i < inputsInUse.Count; i++)
+        {
+            if (inputsInUse[i].GetButtonDown(startGameKey))
+            {
+                TryToStartGame();
+            }
+        }
+
+    }
+
+    private void TryToStartGame()
+    {
+        // collect player selection
+        List<Player> allPlayers = new List<Player>();
+        for (int i = 0; i < allSelectors.Count; i++)
+        {
+            if (allSelectors[i].IsInUse())
+            {
+                allPlayers.Add(new Player(allSelectors[i].GetInput(), allSelectors[i].GetMonsterChoice()));
+            }
+        }
+
+        // start game if sufficient amount of players
+        if (allPlayers.Count >= 2)
+        {
+            GlobalData.SetPlayers(allPlayers);
+            Application.LoadLevel(gameSceneToStartAfterPlayerSelect);
+
+        }
+
+    }
+
+    private void TryToAddPlayer(InputDevice inputDevice)
+    {
+        for (int i = 0; i < allSelectors.Count; i++)
+        {
+
+            if (!allSelectors[i].IsInUse())
+            {
+                allSelectors[i].Show(inputDevice, TryToGetDifferentMonsterChoice(0, true));
+                potentialInputDevices.Remove(inputDevice);
+                inputsInUse.Add(inputDevice);
+
+                break;
+            }
+        }
+    }
+
+
+
+
+
+    internal void RemovePlayer(SingleSelector selector)
+    {
+        potentialInputDevices.Add(selector.GetInput());
+        inputsInUse.Remove(selector.GetInput());
+        selector.Hide();
+    }
+
+    internal int TryToGetDifferentMonsterChoice(int originalChoice, bool biggerChoiceRequested)
+    {
+        // collect all choices
+        List<int> allChoices = new List<int>();
+        for (int i = 0; i < allSelectors.Count; i++)
+        {
+            if (allSelectors[i].IsInUse())
+            {
+                allChoices.Add(allSelectors[i].GetMonsterChoice());
+            }
+        }
+        // sort
+        allChoices.Sort();
+
+        // only 4 choices available
+        if (allChoices.Count == 4)
+        {
+            return originalChoice;
+        }
+        else
+        {
+            // hunt for next best choice
+
+            int startChoice = originalChoice;
+
+            for (int i = 0; i < 4; i++)
+            {
+                // bigger/smaller
+                if (biggerChoiceRequested)
+                {
+                    startChoice++;
+                }
+                else
+                {
+                    startChoice--;
+                }
+
+                // overshoot
+                if (startChoice < 0)
+                {
+                    startChoice = 4;
+                }
+                else if (startChoice > 4)
+                {
+                    startChoice = 1;
+                }
+
+                // check if available
+                if (!allChoices.Contains(startChoice))
+                {
+                    return startChoice;
+                }
+            }
+        }
+
+        // fallback
+        return originalChoice;
+
+    }
+
+}
---
+++ Assets/UI/Player Select/SingleSelector.cs
@@ -0,0 +1,159 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class SingleSelector : MonoBehaviour
+{
+
+    [SerializeField]
+    RectTransform rectTrans;
+
+    [SerializeField]
+    RectTransform widthReferencePoint;
+
+    InputDevice inputUsed;
+
+    PlayerSelectScreen playerSelectScreen;
+
+    [SerializeField]
+    List<GameObject> selectionGraphics;
+
+    [SerializeField]
+    GameObject emptyGraphic;
+
+
+    [SerializeField]
+    Image leftArrow;
+    [SerializeField]
+    Image rightArrow;
+    [SerializeField]
+    Text playerText;
+    [SerializeField]
+    List<Color32> playerColors;
+
+    [SerializeField]
+    int monsterChoice;
+
+    [SerializeField]
+    RectTransform fireToJoinText;
+
+    bool isInUse = false;
+
+
+    public void Initialize(int position, PlayerSelectScreen playerSelectScreen)
+    {
+
+        float completeWidth = widthReferencePoint.localPosition.x * 2f;
+        float widthSteps = completeWidth / 8f;
+        float startWidth = -completeWidth / 2f;
+
+        float widthChoice = startWidth + (1 + position * 2) * widthSteps;
+        float heightChoice = -95;
+        rectTrans.localPosition = new Vector2(widthChoice, heightChoice);
+
+        this.playerSelectScreen = playerSelectScreen;
+
+        playerText.text = "P" + (position + 1).ToString();
+
+        leftArrow.color = playerColors[position];
+        rightArrow.color = playerColors[position];
+        playerText.color = playerColors[position];
+
+
+        float textOffset = ((1920 / 2f) - widthReferencePoint.localPosition.x) / 4f;
+        Debug.Log(widthReferencePoint.localPosition.x);
+        fireToJoinText.localPosition = 
+            new Vector2(fireToJoinText.localPosition.x - textOffset, fireToJoinText.localPosition.y);
+
+    }
+
+
+    public void Show(InputDevice inputUsed, int monsterChoice)
+    {
+        this.monsterChoice = monsterChoice;
+        ShowMonsterChoice();
+
+        this.inputUsed = inputUsed;
+        isInUse = true;
+    }
+
+
+    public void ShowMonsterChoice()
+    {
+        HideAllMonsterGraphics();
+        selectionGraphics[monsterChoice - 1].gameObject.SetActive(true);
+    }
+
+    void HideAllMonsterGraphics()
+    {
+        for (int i = 0; i < selectionGraphics.Count; i++)
+        {
+            selectionGraphics[i].gameObject.SetActive(false);
+        }
+        emptyGraphic.gameObject.SetActive(false);
+    }
+
+    public void Hide()
+    {
+        HideAllMonsterGraphics();
+        emptyGraphic.gameObject.SetActive(true);
+        isInUse = false;
+    }
+
+    public bool IsInUse()
+    {
+        return isInUse;
+    }
+
+
+    bool directionalChoiceInLastFrame = false;
+
+    void Update()
+    {
+        if (isInUse)
+        {
+
+
+            // check for monster Choice change
+            if (directionalChoiceInLastFrame)
+            {
+                if (Mathf.Abs(inputUsed.GetXAxis()) < 0.5f)
+                {
+                    directionalChoiceInLastFrame = false;
+                }
+            }
+            else
+            {
+                if (Mathf.Abs(inputUsed.GetXAxis()) > 0.5f)
+                {
+                    directionalChoiceInLastFrame = true;
+                    bool biggerChoiceRequested = inputUsed.GetXAxis() > 0.5f;
+                    int newChoice = playerSelectScreen.TryToGetDifferentMonsterChoice(monsterChoice, biggerChoiceRequested);
+                    monsterChoice = newChoice;
+                    ShowMonsterChoice();
+                }
+            }
+
+
+
+            // check for quit
+            if (IsInUse() && inputUsed.GetButtonDown(PlayerSelectScreen.leaveKey))
+            {
+                playerSelectScreen.RemovePlayer(this);
+            }
+
+        }
+    }
+
+    public InputDevice GetInput()
+    {
+        return inputUsed;
+    }
+
+    public int GetMonsterChoice()
+    {
+        return monsterChoice;
+    }
+
+
+}
