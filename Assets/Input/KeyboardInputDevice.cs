8bfaecb4a2f30e7a9b9246861e1df3a6e8273cb6
Roman <kampfflummi@gmx.de>
2015-08-24 19:23:19 +0200

    enter / esc key for keyboard as start

--- Assets/Input/KeyboardInputDevice.cs
+++ Assets/Input/KeyboardInputDevice.cs
@@ -1,68 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class KeyboardInputDevice : InputDevice {
+public class KeyboardInputDevice : InputDevice
+{
 
-	override public float GetXAxis()
-	{
-		float x = 0;
+    override public float GetXAxis()
+    {
+        float x = 0;
 
-		if(Input.GetKey(KeyCode.A))
-			x -= 1;
+        if (Input.GetKey(KeyCode.A))
+            x -= 1;
 
-		if(Input.GetKey(KeyCode.D))
-			x += 1;
+        if (Input.GetKey(KeyCode.D))
+            x += 1;
 
-		return x;
-	}
-	
-	override public float GetYAxis()
-	{
-		float y = 0;
-		
-		if(Input.GetKey(KeyCode.S))
-			y -= 1;
-		
-		if(Input.GetKey(KeyCode.W))
-			y += 1;
-		
-		return y;
-	}
-	
-	override public bool GetButton(int buttonIndex)
-	{
-		switch(buttonIndex) {
-			case 0: return Input.GetKey (KeyCode.J);
-			case 1: return Input.GetKey (KeyCode.K);
-			case 2: return Input.GetKey (KeyCode.L);
-			case 3: return Input.GetKey (KeyCode.I);
-		}
+        return x;
+    }
 
-		return false;
-	}
-	
-	override public bool GetButtonDown(int buttonIndex)
-	{
-		switch(buttonIndex) {
-			case 0: return Input.GetKeyDown (KeyCode.J);
-			case 1: return Input.GetKeyDown (KeyCode.K);
-			case 2: return Input.GetKeyDown (KeyCode.L);
-			case 3: return Input.GetKeyDown (KeyCode.I);
-		}
+    override public float GetYAxis()
+    {
+        float y = 0;
 
-		return false;
-	}
-	
-	
-	override public bool GetButtonUp(int buttonIndex)
-	{
-		switch(buttonIndex) {
-			case 0: return Input.GetKeyUp (KeyCode.J);
-			case 1: return Input.GetKeyUp (KeyCode.K);
-			case 2: return Input.GetKeyUp (KeyCode.L);
-			case 3: return Input.GetKeyUp (KeyCode.I);
-		}
+        if (Input.GetKey(KeyCode.S))
+            y -= 1;
 
-		return false;
-	}
+        if (Input.GetKey(KeyCode.W))
+            y += 1;
+
+        return y;
+    }
+
+    override public bool GetButton(int buttonIndex)
+    {
+        switch (buttonIndex)
+        {
+            case 0: return Input.GetKey(KeyCode.J);
+            case 1: return Input.GetKey(KeyCode.K);
+            case 2: return Input.GetKey(KeyCode.L);
+            case 3: return Input.GetKey(KeyCode.I);
+            case 7: return Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.Escape);
+
+        }
+
+        return false;
+    }
+
+    override public bool GetButtonDown(int buttonIndex)
+    {
+        switch (buttonIndex)
+        {
+            case 0: return Input.GetKeyDown(KeyCode.J);
+            case 1: return Input.GetKeyDown(KeyCode.K);
+            case 2: return Input.GetKeyDown(KeyCode.L);
+            case 3: return Input.GetKeyDown(KeyCode.I);
+            case 7: return Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Escape);
+        }
+
+        return false;
+    }
+
+
+    override public bool GetButtonUp(int buttonIndex)
+    {
+        switch (buttonIndex)
+        {
+            case 0: return Input.GetKeyUp(KeyCode.J);
+            case 1: return Input.GetKeyUp(KeyCode.K);
+            case 2: return Input.GetKeyUp(KeyCode.L);
+            case 3: return Input.GetKeyUp(KeyCode.I);
+            case 7: return Input.GetKeyUp(KeyCode.Return) || Input.GetKeyUp(KeyCode.Escape);
+        }
+
+        return false;
+    }
 }
